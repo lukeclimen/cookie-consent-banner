5b743d8f4351950c3ba767340e0b03881f9c4a8a
lukeclimen <luke.climenhage@gmail.com>
2026-06-25 15:38:03 -0700

    Run linter

--- src/counter.test.ts
+++ src/counter.test.ts
@@ -1,9 +1,9 @@
-import {expect, test} from 'vitest';
+import { expect, test } from "vitest";
 
-const sum = ( a:number, b: number ): number => {
-    return a + b;
-}
+const sum = (a: number, b: number): number => {
+  return a + b;
+};
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3)
-})
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
